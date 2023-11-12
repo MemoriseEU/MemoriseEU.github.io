b8f4c4c75e383cd11c51a30a89e67a686a0d8bca
Jakob Kusnick <jakob.kusnick@hotmail.de>
2023-11-12 16:00:17 +0100

    First scenes rendering

--- src/app/model-viewer/[id]/page.tsx
+++ src/app/model-viewer/[id]/page.tsx
@@ -1,15 +1,13 @@
+"use client";
+
 import Model from "@/components/model-viewer/Model";
 
 export default function ModelViewer({ params }: { params: { id: string } }) {
-  const { id } = params;
+  const { id = "scene-1" } = params;
 
   return (
     <div style={{ width: "500px", height: "500px" }}>
       <Model glbSrc={`/assets/${id}.glb`} />
     </div>
   );
-}
-
-export function generateStaticParams() {
-  return [{ id: "scene-1" }, { id: "scene-2" }];
 }
