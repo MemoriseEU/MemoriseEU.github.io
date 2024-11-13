5a24c24e7c6d5c251e83a03e839e858cedadbf09
Jakob Kusnick <jakob.kusnick@hotmail.de>
2024-11-13 15:50:32 +0100

    Fix types

--- src/app/use-idle-timeout.ts
+++ src/app/use-idle-timeout.ts
@@ -4,7 +4,13 @@ import { useIdleTimer } from "react-idle-timer";
  * @param onIdle - function to notify user when idle timeout is close
  * @param idleTime - number of seconds to wait before user is logged out
  */
-const useIdleTimeout = ({ onIdle, idleTime = 1 }) => {
+interface IdleProps {
+  onIdle: () => void;
+  idleTime: number;
+}
+
+const useIdleTimeout = (params: IdleProps) => {
+  const { onIdle, idleTime = 1 } = params;
   const idleTimeout = 1000 * idleTime;
   const [isIdle, setIdle] = useState(false);
   const handleIdle = () => {
